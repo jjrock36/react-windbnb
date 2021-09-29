8aa92380e9b90461bb96fd38fe7382b530e306e5
jjrock36 <jjrock36@gmail.com>
2021-09-29 13:15:56 -0600

    update react-windbnb

--- src/Location.js
+++ src/Location.js
@@ -5,20 +5,27 @@ export const Location = () => {
   const { locations, handleLocation } = useGlobalContext();
 
   return (
-    <ul className="location-content">
-      {locations.map((location, index) => {
-        return (
-          <li key={index} className="location-container">
-            <span className="material-icons-round location-icon">place</span>
+    <>
+      <ul className="location-content">
+        {locations.map((location, index) => {
+          return (
+            <li key={index} className="location-container">
+              <span className="material-icons-round location-icon">place</span>
 
-            <p className="location-text">
-              <span id={location} onClick={handleLocation}>
-                {location}
-              </span>
-            </p>
-          </li>
-        );
-      })}
-    </ul>
+              <p className="location-text">
+                <span id={location} onClick={handleLocation}>
+                  {location}
+                </span>
+              </p>
+            </li>
+          );
+        })}
+      </ul>
+      <div className="clear-location-container">
+        <span className="clear-location" id="clear" onClick={handleLocation}>
+          clear location
+        </span>
+      </div>
+    </>
   );
 };
